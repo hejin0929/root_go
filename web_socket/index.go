ed852dd2d626e1a7e1988949051c5453a6aff524
huanghejin <huanghejin@realibox.com>
2022-04-06 19:37:06 +0800

    修改创建组撤回错序的bug

--- web_socket/index.go
+++ web_socket/index.go
@@ -2,9 +2,11 @@ package web_socket
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	"net/http"
+	"time"
 )
 
 // socket 配置
@@ -41,12 +43,13 @@ var web = websocket.Upgrader{
 type Client struct {
 	Port          string          // 客户端地址
 	Socket        *websocket.Conn // 用户连接
-	Send          chan []byte     // 待发送数据
+	Send          []byte          // 待发送数据
 	APPId         uint            // 登陆平台
 	UserId        string          // 用户ID
 	FirstTime     uint64          // 首次连接时间
 	HeartbeatTime uint64          // 上次心跳
 	LoginTime     uint64          // 登陆时间
+	Read          []byte          // 读取数据
 }
 
 func WebSocketNews(g *gin.Context) (*Client, error) {
@@ -66,6 +69,31 @@ func WebSocketNews(g *gin.Context) (*Client, error) {
 	return &ws, nil
 }
 
-//func ()  {
-//
-//}
+// Start 启动websocket
+func (_this *Client) Start() {
+	_this.FirstTime = uint64(time.Now().Unix())
+
+}
+
+// Sends 发送数据
+func (_this *Client) Sends() {
+	err := _this.Socket.WriteMessage(http.StatusOK, _this.Send)
+	if err != nil {
+		err = _this.Socket.Close()
+		return
+	}
+}
+
+// Reads 读取数据
+func (_this *Client) Reads() {
+	message, read, err := _this.Socket.ReadMessage()
+
+	_this.Read = read
+
+	fmt.Println("this is a ?? ", message, read)
+
+	if err != nil {
+		return
+	}
+}
+
